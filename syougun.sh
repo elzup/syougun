56e0fc9575ccd39a2f781c4596dbb2fe90b42e89
mikekuroe <mike@cps.im.dendai.ac.jp>
2015-10-02 01:08:06 +0900

    this shell use syougun.mp3

---
+++ syougun.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo apt-get install mpg321
+echo '#if use this command. you need syougun.mp3' >> ~/.zshrc
+echo 'alias syougun="mpg321 /home/mike/Music/syougun.mp3 &;git push orgin master:master"' >> ~/.zshrc 
