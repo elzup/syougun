6ef39997eaf69596380e82e846d995cb9e6d870b
mikekuroe <mike@cps.im.dendai.ac.jp>
2015-10-02 01:24:39 +0900

    fix origin spell

--- syougun.sh
+++ syougun.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get install mpg321
 echo '#if use this command. you need syougun.mp3' >> ~/.zshrc
-echo 'alias syougun="mpg321 /home/mike/Music/syougun.mp3 &;git push orgin master:master"' >> ~/.zshrc 
+echo 'alias syougun="mpg321 /home/mike/Music/syougun.mp3 &;git push origin master:master"' >> ~/.zshrc 
