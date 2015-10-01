b4d2017e6d4464e9994c18a1f37fdaf921694b31
mikekuroe <mike@cps.im.dendai.ac.jp>
2015-10-02 02:04:15 +0900

    fix orgin

--- syougun.sh
+++ syougun.sh
@@ -3,4 +3,4 @@
 sudo apt-get install mpg321
 
 echo '#if use this command. you need syougun.mp3' >> ~/$1
-echo 'alias syougun="mpg321 /home/mike/Music/syougun.mp3 &;git push orgin master"' >> ~/$1 
+echo 'alias syougun="mpg321 /home/mike/Music/syougun.mp3 &;git push origin master"' >> ~/$1 
