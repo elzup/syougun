340ee172cf134cd518f224540d233c6c5dda2f2b
mikekuroe <mike@cps.im.dendai.ac.jp>
2015-10-05 15:24:03 +0900

    カレントディレクトリの修正

--- syougun.sh
+++ syougun.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get install mpg321
 
-echo '#if use this command. you need syougun.mp3' >> ~/$1
-echo 'alias syougun="mpg321 /home/mike/Music/syougun.mp3 &;git push origin master -f"' >> ~/$1 
+#echo '#if use this command. you need syougun.mp3' >> ~/$1
+#echo 'alias syougun="mpg321 ~/Music/syougun.mp3 &;git push origin master -f"' >> ~/$1 
