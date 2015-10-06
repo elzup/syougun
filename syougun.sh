f75399fc81dfeb524d87a496634332feac4c2785
elzup <guild0105@gmail.com>
2015-10-06 22:35:22 +0900

    Update syougun.sh

--- syougun.sh
+++ syougun.sh
@@ -3,4 +3,4 @@
 sudo apt-get install mpg321
 
 echo '#if use this command. you need syougun.mp3' >> ~/$1
-echo 'alias syougun="mpg321 ~/Music/syougun.mp3 &;git push origin master"' >> ~/$1 
+echo 'alias syougun="mpg321 ~/Music/syougun.mp3 &;git checkout -b abarenbouI;git checkout -b abarenbouII;git checkout -b abarenbouIII;git checkout -b abarenbouIV;git checkout -b abarenbouV;git checkout -b abarenbouVI;git push origin --all;"' >> ~/$1 
